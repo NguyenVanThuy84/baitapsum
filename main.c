2f5eade2c8e7e06cd227f228ec4454ab239f762e
NguyenVanThuy84 <106532457+NguyenVanThuy84@users.noreply.github.com>
2022-05-30 20:58:16 +0700

    baitapsum

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void) {
+int interger1;
+int interger2;
+int sum;
+printf ("nhap so 1: \n");
+scanf ("%d", &interger1);
+printf ("nhap so 2: \n");
+scanf ("%d", &interger2);
+sum = interger1 + interger2;
+printf("sum = %d", sum);	
+
+}
